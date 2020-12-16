c6da1753565cc45d9ae52ca55dbdb9735584791e
Mastodont <specttre@abv.bg>
2020-12-16 11:30:42 +0000

    error

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 str_1 = input()
-print([int(i) for i in str_1 if i % ])
+# int in the beginning of the list comprehension and in the if otherwsie won't work at all and gives "typeerror: not all arguments converted ... in Python" error.
+print([int(i) for i in str_1 if int(i) % 2 == 1])
