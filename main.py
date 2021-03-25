5c1be309ed001602b1a49a6db48ff1ddf63002cf
Zdravko Stoyanov <71426697+ZdrStr@users.noreply.github.com>
2021-03-25 23:06:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 str_1 = input()
-# int in the beginning of the list comprehension and in the if otherwsie won't work at all and gives "typeerror: not all arguments converted ... in Python" error.
+# int in the beginning of the list comprehension and in the if otherwsie won't work at all and gives "type error: not all arguments converted ... in Python" error.
 print([int(i) for i in str_1 if int(i) % 2 == 1])
